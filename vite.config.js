f7acf4c4e168131e49fd2e586f2d9727e7de9a5e
yubin <yubin.email@163.com>
2025-04-26 23:09:33 +0800

    :rocket: change base url of config

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/LS-Mixture',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
